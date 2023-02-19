fb549af8930bab9daefd2f89c2e301fd3d2c4d45
Владимир Войтенко <vladimirdev635@gmail.com>
2023-02-20 01:31:24 +0500

    fix

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ Copyright © 2023 Vladimir Voitenko vladimirdev635@gmail.com
 */
 package main
 
-import (
-	"fmt"
-	"osiris/cmd"
-)
+import "osiris/cmd"
 
-var Version = "0.0";
 
 func main() {
-	fmt.Println(Version)
 	cmd.Execute()
 }
